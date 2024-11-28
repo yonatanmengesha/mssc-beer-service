f2f75e1b706beeb8dc43ed3324fa74bcb0910a55
yonatanmengesha <yonatan.mengesha2017@gmail.com>
2024-11-28 18:03:56 -0500

    added  bean validation .closes #8

--- src/main/java/guru/springframwork/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframwork/msscbeerservice/web/controller/BeerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/beer")
@@ -18,14 +19,14 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity  saveNewBeer(@RequestBody  BeerDto beerDto){
+    public ResponseEntity  saveNewBeer(@Valid @RequestBody  BeerDto beerDto){
 
         //todo the practical impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity  updateBeerById(@PathVariable("beerId")UUID beerId,@RequestBody BeerDto beerDto){
+    public ResponseEntity  updateBeerById(@PathVariable("beerId")UUID beerId,@Valid @RequestBody BeerDto beerDto){
         //todo the practical impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
